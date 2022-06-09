b85c3b7159cb31768e84aa140f10a6accd6b22b8
DuverH <DuverH@DESKTOP-2SFODPA>
2022-06-09 13:26:47 -0500

    version 2

--- src/githubpreuba/GithubPreuba.java
+++ src/githubpreuba/GithubPreuba.java
@@ -16,6 +16,7 @@ public class GithubPreuba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("version 1");
     }
     
 }
